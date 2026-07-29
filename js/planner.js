d102bde5aec4028188ae9ffd515e4aaa2cb29f58
Syeda Manahil <syedamanahil06@gmail.com>
2026-07-29 07:36:27 +0500

    Update planner.js

--- js/planner.js
+++ js/planner.js
@@ -1,76 +1,120 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-
-<title>Diet Planner</title>
-
-<link rel="stylesheet" href="css/style.css">
-
-</head>
+function generatePlan(){
 
 
-<body>
+let name =
+document.getElementById("userName").value;
 
 
-<header>
-
-<h1>90 Day Diet Planner 🍽️</h1>
-
-</header>
+let age =
+document.getElementById("age").value;
 
 
-
-<section class="hero">
-
-
-<input id="userName" placeholder="Your Name">
+let weight =
+document.getElementById("weight").value;
 
 
-<input id="age" type="number" placeholder="Age">
+let height =
+document.getElementById("height").value;
 
 
-<input id="weight" type="number" placeholder="Weight kg">
-
-
-<input id="height" type="number" placeholder="Height cm">
+let goal =
+document.getElementById("goal").value;
 
 
 
-<select id="goal">
-
-<option value="gain">
-Weight Gain
-</option>
-
-
-<option value="loss">
-Weight Loss
-</option>
-
-
-</select>
+let result =
+document.getElementById("planResult");
 
 
 
-<button onclick="generatePlan()">
+if(name==="" || weight==="" || height===""){
 
-Generate Plan
+result.innerHTML =
 
-</button>
+"<div class='foodCard'>Please fill all details.</div>";
 
+return;
 
-
-<div id="planResult"></div>
-
-
-</section>
+}
 
 
 
-<script src="js/planner.js"></script>
+let bmi =
+weight / ((height/100)*(height/100));
 
 
-</body>
 
-</html>
+let plan;
+
+
+
+if(goal==="gain"){
+
+
+plan = `
+
+<h2>Weight Gain Plan</h2>
+
+<h3>Breakfast</h3>
+<p>Oats + Milk + Banana + Nuts</p>
+
+<h3>Lunch</h3>
+<p>Rice + Chicken + Vegetables</p>
+
+<h3>Brunch/Snack</h3>
+<p>Peanut Butter Sandwich + Milk</p>
+
+<h3>Dinner</h3>
+<p>Egg Omelette + Roti + Yogurt</p>
+
+<p>Daily Target: Increase calories with protein-rich foods.</p>
+
+`;
+
+}
+
+
+else{
+
+
+plan = `
+
+<h2>Weight Loss Plan</h2>
+
+<h3>Breakfast</h3>
+<p>Eggs + Fruits + Green Tea</p>
+
+<h3>Lunch</h3>
+<p>Chicken Salad + Brown Rice</p>
+
+<h3>Brunch/Snack</h3>
+<p>Greek Yogurt + Nuts</p>
+
+<h3>Dinner</h3>
+<p>Soup + Vegetables + Protein</p>
+
+<p>Daily Target: Balanced calories and high nutrition.</p>
+
+`;
+
+}
+
+
+
+result.innerHTML = `
+
+<div class="foodCard">
+
+<h2>${name}'s Plan</h2>
+
+<p><b>Age:</b> ${age}</p>
+
+<p><b>BMI:</b> ${bmi.toFixed(1)}</p>
+
+${plan}
+
+</div>
+
+`;
+
+}
